d44581751dded3186fc1247d00e52edddc49a951
Maxim <mkaramurzin02@gmail.com>
2023-06-25 13:17:18 -0700

    frontend configured and first component implemented

---
+++ mern-exercise-tracker/src/components/edit-exercise.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class EditExercise extends Component {
+  // TODO: Implement exercise edit form.
+  render() {
+    return (
+      <div>
+        <p>Edit Exercise Component!</p>
+      </div>
+    );
+  }
+}
---
+++ mern-exercise-tracker/src/components/exercise-list.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class ExerciseList extends Component {
+  // TODO: Implement exercise list retrieval and display.
+  render() {
+    return (
+      <div>
+        <p>Exercise List Component!</p>
+      </div>
+    );
+  }
+}
